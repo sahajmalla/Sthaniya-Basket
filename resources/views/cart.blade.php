4a817ec448ae310f3546e961f28ee62a52425bbd
Binaya Thapa Magar <binayaekacai96@gmail.com>
2021-06-29 01:21:18 +0545

    Made cart layout like daraz

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -34,17 +34,15 @@
                                     <tr class="h-12 uppercase">
                                         <th class="hidden md:table-cell pb-4"></th>
                                         
-                                        <th class="text-left pb-4">Product</th>
+                                        <th class="text-center pb-4">Product</th>
                                         
-                                        <th class="pb-4 lg:text-right text-left pl-5 lg:pl-0 ">
+                                        <th class="pb-4 text-center pl-4 lg:pl-0 ">
                                             <span class="lg:hidden" title="Quantity">Qtd</span>
                                             <span class="hidden lg:inline">Quantity</span>
                                         </th>
                                         
-                                        <th class="hidden pb-4 text-right md:table-cell">Unit price</th>
-                                        
-                                        <th class="text-right pb-4">Total price</th>
-                                    
+                                        <th class="text-center pb-4">Unit price</th>
+                                                                            
                                     </tr>
                                 </thead>
 
@@ -58,17 +56,18 @@
                                                 <td class="hidden pb-8 md:table-cell">
                                                     <a href="#">
                                                         <img src="/images/products/{{ $product->prod_image }}"
-                                                            class="w-20 rounded" alt="Thumbnail">
+                                                            class="w-28 h-28 rounded" alt="Thumbnail">
                                                     </a>
                                                 </td>
                                             
                                                 <!-- Name and remove button -->
                                                 <td class="pb-8">
                                                         
-                                                    <p class="mb-2 text-md font-medium">{{ $product->prod_name }}</p>
+                                                    <p class="mb-2 text-md text-center font-medium">{{ $product->prod_name }}</p>
                                                     
                                                     <!-- Remove product button -->
-                                                    <form action="{{ route('cart.destroy', $product->id) }}" method="POST">
+                                                    <form action="{{ route('cart.destroy', $product->id) }}" 
+                                                        method="POST" class="flex justify-center">
                                                         @csrf
                                                         @method('DELETE')
                                                         <button type="submit" class="text-white text-sm bg-red-600 hover:bg-red-700 rounded-lg p-1">
@@ -80,8 +79,8 @@
                                                 </td>
                                             
                                                 <!-- Dynamic Quantity -->
-                                                <td class="justify-center md:justify-end md:flex mt-6">
-                                                    <div class="w-20 h-10">
+                                                <td class="justify-center md:flex mt-10">
+                                                    <div class="w-24 h-10">
                                                         <div class="relative flex flex-row w-full h-8">
                                                             <input type="number" id="quantity" value="1"
                                                                 class="w-full font-semibold text-center 
@@ -93,18 +92,11 @@
                                                 </td>
                                             
                                                 <!-- Unit Price -->
-                                                <td class="hidden text-right md:table-cell pb-8">
+                                                <td class="text-center pb-8">
                                                     <span class="text-sm lg:text-base font-medium">
                                                         £{{ $product->price }}
                                                     </span>
-                                                </td>
-                                            
-                                                <!-- Total price -->
-                                                <td class="text-right pb-8">
-                                                    <span class="text-sm lg:text-base font-medium">
-                                                        £{{ $product->price }}
-                                                    </span>
-                                                </td>
+                                                </td>                             
                                             
                                             </tr>
 
@@ -117,58 +109,24 @@
                             <hr class="pb-6 mt-6">
 
                             <!-- Un-Authenticated User's Order Details -->
+
                             <div class="my-4 mt-6 -mx-2 flex justify-center">
 
                                 <div class="lg:px-2 lg:w-1/2">
                                     
-                                    <div class="p-4 bg-gray-100 rounded-full">
-                                        <h1 class="ml-2 font-bold uppercase">Order Details</h1>
-                                    </div>
-                                    
-                                    <div class="p-4">
-
-                                        {{-- <p class="mb-6 italic">Total price may vary depending on the discount rate.</p> --}}
-                                        
-                                        <div class="flex justify-between border-b">
-                                            <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                                                <p>Total Items</p>
-                                            </div>
-                                            <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                                                <p>{{ count(session('products')) }}</p>
-                                            </div>
-                                        </div>
-                                        
-                                        <div class="flex justify-between pt-4 border-b">
-                                            <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                                                <p>Total Quantity</p>
-                                            </div>
-                                            <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                                                <p id="total-quantity">10</p>
-                                            </div>
-                                        </div>
-
-                                        <div class="flex justify-between pt-4 border-b">
-                                            <div class="lg:px-4 lg:py-2 m-2 text-lg lg:text-xl font-bold text-center text-gray-800">
-                                                <p>Total Price</p>
-                                            </div>
-                                            <div class="lg:px-4 lg:py-2 m-2 lg:text-lg font-bold text-center text-gray-900">
-                                                £{{ $total_price }}
-                                            </div>
-                                        </div>
-
-                                        <a href="{{ route('checkout') }}">
-                                            <button
-                                                class="flex justify-center w-full px-10 py-3 mt-6 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
-                                                <svg aria-hidden="true" data-prefix="far" data-icon="credit-card" class="w-8"
-                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                                                    <path fill="currentColor"
-                                                        d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z" />
-                                                </svg>
-                                                <span class="ml-2 mt-5px">Procceed to checkout</span>
-                                            </button>
-                                        </a>
-
-                                    </div>
+                                    <a href="{{ route('checkout') }}">
+                                        <button
+                                            class="flex justify-center w-full px-10 py-3 font-medium text-white uppercase bg-gray-800 rounded-full shadow item-center hover:bg-gray-700 focus:shadow-outline focus:outline-none">
+                                            <svg aria-hidden="true" data-prefix="far" data-icon="credit-card" class="w-8"
+                                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                                <path fill="currentColor"
+                                                    d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48.1 48h479.8c26.6 0 48.1-21.5 48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3 0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8 352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6 6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4 12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12 5.4 12 12z" />
+                                            </svg>
+                                            <span class="ml-2 mt-5px">Procceed to checkout</span>
+                                        </button>
+                                    </a>
+    
+                                </div>
                             </div>
 
                         @else
@@ -244,7 +202,7 @@
                                                 </td>
 
                                                 <!-- Dynamic Quantity -->
-                                                <td class="justify-center md:flex md:mt-10">
+                                                <td class="justify-center md:flex mt-10">
                                                     <div class="w-24 h-10">
                                                         <div class="relative flex flex-row w-full h-8">
                                                             <input name="quantity" id="quantity" type="number" value='1' min="1"
