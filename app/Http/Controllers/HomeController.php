7bb49396d66ae4086b0e8c29c237db129f4ef862
Binaya Thapa Magar <binayaekacai96@gmail.com>
2021-07-10 19:13:42 +0545

    Added collection days and time slots insertion code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,72 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\CollectionDay;
 
 class HomeController extends Controller
 {
     public function index(Request $request) {
+
+        $collectionDays = CollectionDay::get();
+
+        if ($collectionDays->count() === 0) {
+
+            CollectionDay::create([
+                'day'=>'Wednesday'
+            ]);
+            CollectionDay::create([
+                'day'=>'Thursday'
+            ]);
+            CollectionDay::create([
+                'day'=>'Friday'
+            ]);
+    
+
+            $wednesday=CollectionDay::find(1);
+            $thursday=CollectionDay::find(2);
+            $friday=CollectionDay::find(3);
+    
+            $wednesday->collectionTimeSlots()->create([
+                'slot_time' => '10-13',
+                'order_quantity' => 0,
+            ]);
+            $wednesday->collectionTimeSlots()->create([
+                'slot_time' => '13-16',
+                'order_quantity' => 0,
+            ]);
+            $wednesday->collectionTimeSlots()->create([
+                'slot_time' => '16-19',
+                'order_quantity' => 0,
+            ]);
+    
+            $thursday->collectionTimeSlots()->create([
+                'slot_time' => '10-13',
+                'order_quantity' => 0,
+            ]);
+            $thursday->collectionTimeSlots()->create([
+                'slot_time' => '13-16',
+                'order_quantity' => 0,
+            ]);
+            $thursday->collectionTimeSlots()->create([
+                'slot_time' => '16-19',
+                'order_quantity' => 0,
+            ]);
+    
+            $friday->collectionTimeSlots()->create([
+                'slot_time' => '10-13',
+                'order_quantity' => 0,
+            ]);
+            $friday->collectionTimeSlots()->create([
+                'slot_time' => '13-16',
+                'order_quantity' => 0,
+            ]);
+            $friday->collectionTimeSlots()->create([
+                'slot_time' => '16-19',
+                'order_quantity' => 0,
+            ]);
+
+        }
+
         $products = Product::orderByRaw('dbms_random.value')->paginate(20); //for oracle
         // $products = Product::inRandomOrder()->paginate(20); //for mysql
         if($request->sort=='latest'){
